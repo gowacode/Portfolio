254444292569d2a0b59e74a099361d1832936cf8
gowacode <watchgowa@gmail.com>
2025-03-28 18:15:21 -0400

    Url change to client.js

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 let counterContainer = document.querySelector(".website-counter");
-fetch("http://127.0.0.1:3000/count")
+fetch("https://gowacode.github.io/Portfolio/")
 .then(response=>response.json())
 .then(data=>{
     counterContainer.innerHTML = data.count;
